88800a1b8dca1dd9e18133ba9d1bc956109d4042
PranitRout07 <pranitrout72@gmail.com>
2024-04-05 19:45:50 +0530

    updated the code and build name

--- Interacting-with-pods-and-deployment/main.go
+++ Interacting-with-pods-and-deployment/main.go
@@ -4,34 +4,35 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"log"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func main() {
-	kubeconfig := flag.String("kubeconfig", "C:\\Users\\prani\\.kube\\config", "Kuberconfig location")   //kubeconfig path 
+	kubeconfig := flag.String("kubeconfig", "C:\\Users\\prani\\.kube\\config", "Kuberconfig location") //kubeconfig path
 
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 
 	if err != nil {
-		fmt.Println("Error : ", err)
+		log.Fatal("Error : ", err)
 	}
 	clientset, err := kubernetes.NewForConfig(config)
 
 	if err != nil {
-		fmt.Println("Error : ", err)
+		log.Fatal("Error : ", err)
 	}
 
 	ctx := context.Background()
-	pods , err := clientset.CoreV1().Pods("default").List(ctx,metav1.ListOptions{})
-	if err!=nil{
-		fmt.Println("Error : ", err)
+	pods, err := clientset.CoreV1().Pods("default").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		log.Fatal("Error : ", err)
 	}
-	fmt.Printf("\n%s\t %s","CreationTime","Pod Name")
-	
-	for _,pod := range pods.Items {
-		fmt.Printf("\n%s\t %s",pod.CreationTimestamp,pod.Name)	
+	fmt.Printf("\n%s\t %s", "CreationTime", "Pod Name")
+
+	for _, pod := range pods.Items {
+		fmt.Printf("\n%s\t %s", pod.CreationTimestamp, pod.Name)
 	}
 }
